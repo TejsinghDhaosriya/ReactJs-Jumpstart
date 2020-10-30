a10f66f9321748ff9ce091d9a7efea9baeb70af7
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-10-30 14:52:17 +0530

    jump

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import ReactDom from 'react-dom';
+
+/*
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1 className="App">
+      Hello World -Welcome to react world</h1>
+    <Header/>
+    <Footer/>
     </div>
   );
 }
+
+function Header(){
+  return(
+    <h1>Header Part</h1>
+  )
+}
+
+function Footer()
+{
+  return(
+    <h1>Footer Part</h1>
+  )
+}
+*/
+// ReactDom.render(<App/>,document.getElementById('root'));
+class App extends React.Component{
+
+constructor(){
+  super();
+  this.state={
+    count:0
+  }
+} 
+btnClick(){
+  this.setState({
+    count:this.state.count+1
+  })
+} 
+render(){
+  return(
+   <div>
+   <h1>Hello</h1>
+   <h1>Value is {this.state.count}</h1>
+   <button onClick={this.btnClick.bind(this)}>Increase</button>
+
+   </div>
+
+
+
+  )  
+
+}
+
+}
+
 
 export default App;
