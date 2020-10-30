f8f6b34cc1b6a0281bf8863674aa8d7b7c78e72a
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-10-30 15:24:20 +0530

    jump

---
+++ src/About/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrouserRouter , Route,Link }  from 'react-router-dom';
+
+class About extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>About</h1>
+            </div>
+        )
+    }
+}
+
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 // import ReactDom from 'react-dom';
 
+import { BrowserRouter as Router , Route,Link,Switch }  from 'react-router-dom';
+import Home from './Home/home';
+import About from './About/about';
 /*
 
 
@@ -45,10 +48,26 @@ btnClick(){
 render(){
   return(
    <div>
+   <Router>
+     <div>
+       <ul>
+         <li>
+           <Link to={'/'}>Home</Link>}
+         </li>
+         <li>
+           <Link to={'/about'}>About</Link>}
+         </li>
+       </ul>
+       <Switch>
+         <Route exact path='/' component={Home} />
+         <Route exact path='/about' component={About}/>
+       </Switch>
+     </div>
+   </Router>
    <h1>Hello</h1>
    <h1>Value is {this.state.count}</h1>
    <button onClick={this.btnClick.bind(this)}>Increase</button>
-
+  
    </div>
 
 
